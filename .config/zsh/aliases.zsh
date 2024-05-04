55c0815dafc39c37740e9f69ad1f3a411a1b3cda
Nikolay Blagoev <nikolay.blagoev@gmail.com>
2024-05-04 18:27:43 +0300

    Remove unused and fix broken aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias da="direnv allow"
 alias dot='/opt/homebrew/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias navi='navi --path ~/.config/navi'
 
+alias ls='eza --color=always --no-filesize --icons=always --no-time --no-user --no-permissions'
 alias ll='eza -ghla --classify=auto --group-directories-first --git'
 alias lt='eza -ghlT --classify=auto --git'
 alias lx='eza * --color always --icons=always --tree --group-directories-first --git-ignore'
@@ -17,8 +18,6 @@ alias ..='cd ../'                           # Go back 1 directory level
 alias ...='cd ../../'                       # Go back 2 directory levels
 alias .3='cd ../../../'                     # Go back 3 directory levels
 alias .4='cd ../../../../'                  # Go back 4 directory levels
-alias .5='cd ../../../../../'               # Go back 5 directory levels
-alias .6='cd ../../../../../../'            # Go back 6 directory levels
 
 # Get week number
 alias week='date +%V'
@@ -32,12 +31,9 @@ alias localip="sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | gr
 alias ips="sudo ifconfig -a | grep -o 'inet6\\? \\(addr:\\)\\?\\s\\?\\(\\(\\([0-9]\\+\\.\\)\\{3\\}[0-9]\\+\\)\\|[a-fA-F0-9:]\\+\\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
 # URL-encode strings
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+alias urlencode='python3 -c "import sys, urllib.parse as p; print(p.quote_plus(sys.argv[1]));"'
 
 alias path='echo $PATH | tr -s ":" "\n"'
-alias serve='http-server' # npm install http-server
-alias json='python -m json.tool'
-#alias clip='clip.exe'
 alias npm-exec='PATH=$(npm bin):$PATH'
 alias clip='pbcopy'
 alias findP="ps -ef | grep -v grep | grep "
@@ -47,12 +43,6 @@ alias weather="curl -4 http://wttr.in/Sofia"
 # Lists the ten most used commands.
 alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
 
-# Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | xclip -selection clipboard | echo '=> Public key copied to clipboard.'"
-
-# Pipe my private key to my clipboard.
-alias prikey="more ~/.ssh/id_rsa | xclip -selection clipboard | echo '=> Private key copied to clipboard.'"
-
 # tmux
 alias tn='tmux new -s "${$(basename `PWD`)//./}" || tmux at -t "${$(basename `PWD`)//./}"'
 alias attach="tmux attach -t"
@@ -60,8 +50,3 @@ alias clearTmux="clear && printf '\e[3J'"
 
 # Download file with original filename
 alias get="curl -O -L"
-
-# Show $PATH in readable view
-alias path='echo -e ${PATH//:/\\n}'
-
-#alias code="code-insiders"
