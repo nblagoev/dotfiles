8012c968229ec80a7500699c8b81194db22343f2
Nikolay Blagoev <nikolay.blagoev@gmail.com>
2024-12-21 17:45:08 +0200

    Update yazi themes and plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,36 @@
+THEME.git = THEME.git or {}
+
+THEME.git.added_sign = "A"
+THEME.git.deleted_sign = "D"
+THEME.git.modified_sign = "M"
+THEME.git.ignored_sign = "I"
+THEME.git.untracked_sign = "?"
+THEME.git.updated_sign = "U"
+
+THEME.git.added = ui.Style():fg("green"):bold()
+THEME.git.deleted = ui.Style():fg("red"):bold()
+THEME.git.modified = ui.Style():fg("yellow"):bold()
+THEME.git.ignored = ui.Style():fg("white")
+THEME.git.untracked = ui.Style():fg("gray")
+THEME.git.updated = ui.Style():fg("magenta"):bold()
+
 require("folder-rules"):setup()
+require("git"):setup()
+require("githead"):setup({
+  show_branch = true,
+  branch_prefix = "on",
+  branch_color = "blue",
+  branch_symbol = "",
+  branch_borders = "()",
+
+  commit_color = "bright magenta",
+  commit_symbol = "@",
+
+  show_behind_ahead = false,
+  show_stashes = false,
+  show_state = false,
+  show_staged = false,
+  show_unstaged = false,
+  show_untracked = false,
+})
+
